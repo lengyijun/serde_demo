d79e4be8f5706301378fb969f4edfe7ef40540b2
lengyijun <sjtu5140809011@gmail.com>
2023-12-23 09:13:41 +0800

    sm2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+use chrono::{prelude::*, Duration};
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 
@@ -10,7 +11,9 @@ struct Sm {
     /// how "easy" the card is (more precisely, it determines how quickly the inter-repetition interval grows)
     ef: f32,
     ///  is the length of time (in days) SuperMemo will wait after the previous review before asking the user to review the card again.
-    l: u32,
+    interval: u32,
+
+    last_reviewed: DateTime<Local>,
 }
 
 impl Default for Sm {
@@ -19,7 +22,46 @@ impl Default for Sm {
         Self {
             n: 0,
             ef: 2.5,
-            l: 1,
+            interval: 1,
+            last_reviewed: Local::now(),
+        }
+    }
+}
+
+impl Sm {
+    fn next_review_time(&self) -> DateTime<Local> {
+        self.last_reviewed + Duration::days(self.interval.try_into().unwrap())
+    }
+
+    // requires{1 <= user_grade <= 5}
+    fn sm2(&self, user_grade: u8) -> Self {
+        let n: u32;
+        let I: u32;
+
+        if user_grade >= 3 {
+            if self.n == 0 {
+                I = 1;
+            } else if self.n == 1 {
+                I = 6;
+            } else {
+                I = (self.interval as f32 * self.ef).round() as u32;
+            }
+            n = self.n + 1;
+        } else {
+            I = 1;
+            n = 0;
+        }
+
+        let mut ef =
+            self.ef + (0.1 - (5 - user_grade) as f32 * (0.08 + ((5 - user_grade) as f32) * 0.02));
+        if ef < 1.3 {
+            ef = 1.3;
+        }
+        Self {
+            n,
+            ef,
+            interval: I,
+            last_reviewed: Local::now(),
         }
     }
 }
